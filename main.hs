ccf9ab4ff9a4f3f1241690c7796bb2be83cb3cae
Bearice Ren <bearice@gmail.com>
2016-04-09 02:33:57 +0800

    allow listen on both tcp and unix sockets

--- main.hs
+++ main.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Main where
 import System.Directory (removeFile)
@@ -8,31 +7,46 @@ import System.Environment
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger
 import Network.Socket
+import Data.Maybe
+import Data.List
 import Data.Default.Class (def)
 import Control.Exception (bracket)
 
+data ServerSocketAddress = UnixSocket String | TCPSocket Int
+
 main :: IO()
-main = bracket createSocket destroySocket $ \socket -> do
+main = bracket createSocket destroySocket $ \(_,socket) -> do
     listen socket 10
     scottySocket def socket routing
   where
-    sockPath = do
-        env <- getEnvironment
-        return $ maybe "./ipam.sock" id $ lookup "LISTEN" env
+    listenOn = do
+      env <- getEnvironment
+      let val = fromMaybe "./ipam.sock" $ lookup "LISTEN" env
+      return $ if "unix://" `isPrefixOf` val
+        then
+          addrInfo AF_UNIX $ SockAddrUnix $ drop 7 val
+        else
+          addrInfo AF_INET $ SockAddrInet (fromInteger $ read val) 0
+        where
+          addrInfo af addr =
+            AddrInfo { addrFamily = af, addrAddress = addr,
+            addrSocketType =Stream, addrFlags = [],
+            addrProtocol = 0, addrCanonName = Nothing }
 
     createSocket = do
-        path <- sockPath
-        sock <- socket AF_UNIX Stream 0
-        bind sock $ SockAddrUnix path
-        return sock
+      addr <- listenOn
+      print addr
+      sock <- socket (addrFamily addr) Stream 0
+      bind sock (addrAddress addr)
+      return (addr,sock)
 
-    destroySocket sock = do
-        path <- sockPath
-        removeFile path
-        close sock
+    destroySocket (addr,sock) = do
+      address <- listenOn
+      close sock
+      case addrAddress address of
+        SockAddrUnix path -> removeFile path
+        _ -> return ()
 
     routing = do
-        middleware logStdoutDev
-        get "/" $ text "Hello, world!"
-
-
+      middleware logStdoutDev
+      get "/" $ text "Hello, world!"
