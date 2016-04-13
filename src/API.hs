bf9590ab243ad283b9e0a777e4ac52f81bd60b84
Bearice Ren <bearice@gmail.com>
2016-04-13 11:42:42 +0800

    impelents ipamdriver

--- src/API.hs
+++ src/API.hs
@@ -9,6 +9,7 @@ import Data.List
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger
 import Data.Aeson hiding (json)
+import Data.Aeson.Types (emptyObject)
 import Data.Vector as V
 import Data.HashMap.Strict as M
 
@@ -18,7 +19,7 @@ routing = do
   get  "/" (text "Hello, world!")
 
   post "/Plugin.Activate" (mkJson [
-      ("Implements", Array ["NetworkDriver"])
+      ("Implements", Array ["NetworkDriver","IpamDriver"])
     ])
 
   post "/NetworkDriver.GetCapabilities" (mkJson [
@@ -32,6 +33,30 @@ routing = do
   post "/NetworkDriver.EndpointOperInfo" $ mkError "not impemenentd"
   post "/NetworkDriver.Join" $ mkError "not impemenentd"
   post "/NetworkDriver.Leave" $ mkError "not impemenentd"
+
+  post "/IpamDriver.GetCapabilities" $ mkJson [
+      ("RequiresMACAddress", Bool True)
+    ]
+
+  post "/IpamDriver.GetDefaultAddressSpaces" $ mkJson [
+      ("LocalDefaultAddressSpace","10.16.0.0/16"),
+      ("GlobalDefaultAddressSpace","10.16.0.0/16")
+    ]
+
+  post "/IpamDriver.RequestPool" $ mkJson [
+      ("PoolID", "123"),
+      ("Pool",   "10.16.0.0/24"),
+      ("Data",   emptyObject)
+    ]
+
+  post "/IpamDriver.ReleasePool" $ json emptyObject
+
+  post "/IpamDriver.RequestAddress" $ mkJson [
+      ("Address","10.16.0.1/24"),
+      ("Data",emptyObject)
+    ]
+
+  post "/IpamDriver.ReleaseAddress" $ json emptyObject
 
   notFound $ mkError "not found"
 
